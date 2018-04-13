2825b1d104c0df0aab161345606946731480581b
Wilfried Chauveau <wilfried.chauveau@ithinuel.me>
2018-04-13 21:00:48 +0100

    add imagemin at startup and move asserts to site directory

--- gulpfile.js
+++ gulpfile.js
@@ -79,7 +79,9 @@ gulp.task('imagemin', function() {
 	return gulp.src('src/img/**/*.{jpg,png,gif}')
 		.pipe(plumber())
 		.pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
-		.pipe(gulp.dest('assets/img/'));
+		.pipe(gulp.dest('assets/img/'))
+		.pipe(browserSync.reload({stream:true}))
+		.pipe(gulp.dest('_site/assets/img/'));
 });
 
 /**
@@ -97,4 +99,4 @@ gulp.task('watch', function () {
  * Default task, running just `gulp` will compile the sass,
  * compile the jekyll site, launch BrowserSync & watch files.
  */
-gulp.task('default', ['js', 'stylus', 'browser-sync', 'watch']);
+gulp.task('default', ['js', 'stylus', 'imagemin', 'browser-sync', 'watch']);
