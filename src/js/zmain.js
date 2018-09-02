3ff603f98f21c9e30546d18f8d15af7777b6f103
Wilfried Chauveau <wilfried.chauveau@ithinuel.me>
2018-09-02 22:23:04 +0100

    update site face

--- src/js/zmain.js
+++ src/js/zmain.js
@@ -1,106 +1,118 @@
 (function( $, window, undefined ) {
-  // Menu
-  $("a#slide").click(function(){
-    $("#sidebar,a#slide,#fade").addClass("slide");
-    $("#open").hide();
-    $("#search").hide();
-  });
+    // Menu
+    $("a#slide").click(function(){
+        $("#sidebar,a#slide,#fade").addClass("slide");
+        $("#open").hide();
+        $("#search").hide();
+    });
 
-  function closeMenu() {
-    $("#sidebar,a#slide,#fade").removeClass("slide");
-    $("#open").show();
-    $("#search").show();
-  }
-
-  $("#fade").click(closeMenu);
-
-  //Remove space scroll
-  window.onkeydown = function(e) {
-    if(e.keyCode == 32 && e.target == document.body) {
-        e.preventDefault();
-        return false;
+    function closeMenu() {
+        $("#sidebar,a#slide,#fade").removeClass("slide");
+        $("#open").show();
+        $("#search").show();
     }
-  };
-  //Keys
-  $(document).keydown(function(e){
-    //console.log(e.code);
-    if(! $('.search-form').hasClass('active')){
-      switch(e.code) {
-        case "Space":
-        case "Escape":
-          if ($('#fade').hasClass('slide')) {
-            $('#fade').trigger('click');
-          } else {
-            $('a#slide').trigger('click');
-          }
-          break;
-      }
-    }
-    //sidebar active
-    if($('#sidebar').hasClass('slide')){
-      switch(e.code) {
-        case "Digit1":
-          $("#sidebar ul li:first-child a").trigger('click');
-          break;
-        case "Digit2":
-          $("#sidebar ul li:nth-child(2) a").trigger('click');
-          break;
-        case "Digit3":
-          $("#sidebar ul li:nth-child(3) a").trigger('click');
-          break;
-        case "Digit4":
-          $("#sidebar ul li:nth-child(4) a").trigger('click');
-          break;
-        case "Digit5":
-          $("#sidebar ul li:nth-child(5) a").trigger('click');
-          break;
-        case "KeyG":
-          $("#sidebar ul:nth-child(2) li:first-child a").trigger('click');
-          break;
-        case "KeyS":
-          $('#fade').trigger('click');
-          $("#search").trigger('click');
-          break;
-         case "KeyT":
-          $("#sidebar ul:nth-child(2) li:nth-child(3) a").trigger('click');
-          break;
-      }
-      return false;
-    }
-    if($('.search-form').hasClass('active')){
-      switch(e.code) {
-        case "Escape":
-          $('.icon-remove-sign').trigger('click');
-          break;
-      }
-    }
-  })
-  // Search
-  var bs = {
-    close: $(".icon-remove-sign"),
-    searchform: $(".search-form"),
-    canvas: $("body"),
-    dothis: $('.dosearch')
-  };
 
-  bs.dothis.on('click', function() {
-    $('.search-wrapper').toggleClass('active');
-    bs.searchform.toggleClass('active');
-    bs.searchform.find('input').focus();
-    bs.canvas.toggleClass('search-overlay');
-    $('.search-field').simpleJekyllSearch();
-  });
+    $("#fade").click(closeMenu);
 
-  bs.close.on('click', function() {
-    $('.search-wrapper').toggleClass('active');
-    bs.searchform.toggleClass('active');
-    bs.canvas.removeClass('search-overlay');
-  });
+    //Remove space scroll
+    window.onkeydown = function(e) {
+        if(e.keyCode == 32 && e.target == document.body) {
+            e.preventDefault();
+            return false;
+        }
+    };
+    //Keys
+    $(document).keydown(function(e){
+        if(! $('.search-form').hasClass('active')){
+            switch(e.code) {
+                case "Space":
+                case "Escape":
+                    if ($('#fade').hasClass('slide')) {
+                        $('#fade').trigger('click');
+                    } else {
+                        $('a#slide').trigger('click');
+                    }
+                    break;
+            }
+        }
+        //sidebar active
+        if($('#sidebar').hasClass('slide')){
+            switch(e.code) {
+                case "Digit1":
+                    $("#sidebar ul li:first-child a").trigger('click');
+                    break;
+                case "Digit2":
+                    $("#sidebar ul li:nth-child(2) a").trigger('click');
+                    break;
+                case "Digit3":
+                    $("#sidebar ul li:nth-child(3) a").trigger('click');
+                    break;
+                case "Digit4":
+                    $("#sidebar ul li:nth-child(4) a").trigger('click');
+                    break;
+                case "Digit5":
+                    $("#sidebar ul li:nth-child(5) a").trigger('click');
+                    break;
+                case "Digit6":
+                    $("#sidebar ul li:nth-child(6) a").trigger('click');
+                    break;
+                case "Digit7":
+                    $("#sidebar ul li:nth-child(7) a").trigger('click');
+                    break;
+                case "KeyG":
+                    $("#github").trigger('click');
+                    break;
+                case "KeyS":
+                    $("#menu-search").trigger('click');
+                    break;
+                case "KeyT":
+                    $("#twitter").trigger('click');
+                    break;
+                case "KeyK":
+                    $("#keybase").trigger('click');
+                    break;
+            }
+            return false;
+        }
+        if($('.search-form').hasClass('active')){
+            switch(e.code) {
+                case "Escape":
+                    $('.icon-remove-sign').trigger('click');
+                    break;
+            }
+        }
+    })
+    // Search
+    $('#menu-search').on('click', function() {
+        $('#fade').trigger('click');
+        $('#search').trigger('click');
+    });
+    var bs = {
+        close: $(".icon-remove-sign"),
+        searchform: $(".search-form"),
+        canvas: $("body"),
+        dothis: $('.dosearch')
+    };
 
-/*
-  // Scroll
+    bs.dothis.on('click', function() {
+        $('.search-wrapper').toggleClass('active');
+        bs.searchform.toggleClass('active');
+        bs.searchform.find('input').focus();
+        bs.canvas.toggleClass('search-overlay');
+        $('.search-field').simpleJekyllSearch();
+    });
+
+    bs.close.on('click', function() {
+        $('.search-wrapper').toggleClass('active');
+        bs.searchform.toggleClass('active');
+        bs.canvas.removeClass('search-overlay');
+    });
+
+    /*
+        // Scroll
   smoothScroll.init({
     updateURL: false
   })
-*/
+  */
+
 })( Zepto, window );
